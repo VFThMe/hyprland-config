6029b5553b1720a4ff1014b0321d037da3ab9637
VFThMe <amirhanakbaraliev1@gmail.com>
2025-09-23 02:49:26 +0600

    Delete .config directory

--- .config/fish/functions/fish_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_prompt
-    set_color "#d65d0e"
-    echo -n "& "
-    set_color normal
-end
--- .config/hypr/scripts/volume-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Увеличить громкость на 5%
-pactl set-sink-volume @DEFAULT_SINK@ +5%
-# Получить текущую громкость
-VOL=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+%' | head -1)
-# Показать OSD через wob
-echo "$VOL" | wob -p 20 -a center
-
